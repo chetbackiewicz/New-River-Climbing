da32a514697642ba98032d53c1da225eaf38d4ca
Chet Backiewicz <chetbackiewicz@gmail.com>
2022-04-15 12:24:02 -0400

    Added MyClimbs Submission on Routes Page

--- vue/src/components/LogList.vue
+++ vue/src/components/LogList.vue
@@ -50,14 +50,14 @@ table {
   font-family: 'Open Sans', sans-serif;
   width: 80%;
   border-collapse: collapse;
-  border: 3px solid #44475C;
+  border: 3px solid rgb(197, 128, 0);
   margin: 10px 10px 0 10px;
 }
 
 table th {
   text-transform: uppercase;
   text-align: left;
-  background: #44475C;
+  background: rgb(197, 128, 0);
   color: #FFF;
   padding: 8px;
   min-width: 30px;
@@ -66,12 +66,12 @@ table th {
 table td {
   text-align: left;
   padding: 8px;
-  border-right: 2px solid #7D82A8;
+  border-right: 2px solid #b88933;
 }
 table td:last-child {
   border-right: none;
 }
 table tbody tr:nth-child(2n) td {
-  background: #D4D8F9;
+  background: #f1cbab;
 }
 </style>
--- vue/src/views/RouteInfo.vue
+++ vue/src/views/RouteInfo.vue
@@ -5,39 +5,43 @@
           {{$store.state.routeInfo.route_name}} {{$store.state.routeInfo.grade}}
         </h1>
       </header>
-      <div id="route-info">
-        <h3>
-        Rating:
-         <img class = "star-img" src="../assets/star.png" alt="Number of stars" v-for="star in numberOfStars" v-bind:key="star.id">
-      </h3>
-      </div>
-      <div class="type">
-        <h3>
-        Type:
-      </h3>
-      <p v-if="$store.state.routeInfo.sport_trad == 'S'">Sport</p>
-      <p v-else>Trad</p>
-      </div>
-      <div class="height">
-         <h3>
-        Height:
-      </h3>
-      <p>{{$store.state.routeInfo.height}} ft</p>
-      </div>
-      <div class="protection">
-        <h3>Protection:</h3>
-        <p class="bolts">{{$store.state.routeInfo.bolt_count}} bolts,</p>
-        <p v-if="$store.state.routeInfo.has_anchors == true" class="anchors">bolt anchors</p>
-        <p v-else>No Anchors</p>
-      </div>
-      <div class="route-description">
-        <h3>
-        Description 
-      </h3>
-      <p>{{$store.state.routeInfo.description}}</p>
-      </div>
-      <div>
-        <log-form />
+      <div class="top-of-page-display">
+        <div id="route-information">
+          <div id="rating">
+          <h3>
+          Rating:
+          <img class = "star-img" src="../assets/star.png" alt="Number of stars" v-for="star in numberOfStars" v-bind:key="star.id">
+          </h3>
+          </div>
+          <div class="type">
+            <h3>
+            Type:
+          </h3>
+          <p v-if="$store.state.routeInfo.sport_trad == 'S'">Sport</p>
+          <p v-else>Trad</p>
+          </div>
+          <div class="height">
+            <h3>
+            Height:
+          </h3>
+          <p>{{$store.state.routeInfo.height}} ft</p>
+          </div>
+          <div class="protection">
+            <h3>Protection:</h3>
+            <p class="bolts">{{$store.state.routeInfo.bolt_count}} bolts,</p>
+            <p v-if="$store.state.routeInfo.has_anchors == true" class="anchors">bolt anchors</p>
+            <p v-else>No Anchors</p>
+          </div>
+          <div class="route-description">
+            <h3>
+            Description 
+          </h3>
+          <p>{{$store.state.routeInfo.description}}</p>
+          </div>
+        </div>
+        <div id="log-form">
+          <log-form />
+        </div>
       </div>
         <route-item />
   </div>
@@ -75,6 +79,23 @@ export default {
 </script>
 
 <style>
+
+.top-of-page-display {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-areas: 
+  "route-information log-form";
+}
+
+#route-information {
+  grid-area: route-information;
+  padding-top: 2rem;
+}
+
+#log-form {
+  grid-area: log-form;
+  padding-top: 2rem;
+}
 
 .star-img {
   height: 1em;
@@ -114,7 +135,6 @@ export default {
 
 .route-description {
   display: block;
-  max-width: 50%;
 }
 
 
