89b4ac9f768512917410178d11cad72a44063c5d
Chet Backiewicz <chetbackiewicz@gmail.com>
2022-04-16 15:01:02 -0400

    Forecast added to home page

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="home">
     <h1>New River Gorge Guide Home</h1>
+    <weather-forecast />
   </div>
 </template>
 
 <script>
+import WeatherForecast from '@/components/WeatherForecast'
 
 export default {
   name: "home",
+  components: {
+    WeatherForecast
+  },
+  async created() {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=37.8756699&lon=-81.0775981&exclude=hourly,minutely,alerts&units=imperial&appid=6552a8974c91aa8d49b6f50421a35530")
+    const weatherInfo = await response.json();
+    this.$store.commit('SET_WEATHER_INFO', weatherInfo)
+  }
 };
 </script>
+
+
