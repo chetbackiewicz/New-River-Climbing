0c8c77ddacf632f49e784654b0225d0e60cc0652
Chet Backiewicz <chetbackiewicz@gmail.com>
2022-04-17 17:12:22 -0400

    Fixed weather lag

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
     <h1>New River Gorge Guide Home</h1>
-    <weather-forecast />
-    <div  id="map">
+    <weather-forecast v-if="isLoaded" />
+    <h1 v-else>Waiting for weather...</h1>
+    <div id="map">
       <iframe src="https://www.google.com/maps/d/embed?mid=1vEtPPeF_gw3tBivLNxUV2ALDUZjhx7pf&ehbc=2E312F" width="640" height="480"></iframe>
     </div>
   </div>
@@ -16,10 +17,21 @@ export default {
   components: {
     WeatherForecast
   },
+  data() {
+    return {
+      weatherLoaded: false
+    }
+  },
+  computed: {
+    isLoaded() {
+      return this.weatherLoaded;
+    }
+  },
   async created() {
     const response = await fetch("https://api.openweathermap.org/data/2.5/onecall?lat=37.8756699&lon=-81.0775981&exclude=hourly,minutely,alerts&units=imperial&appid=6552a8974c91aa8d49b6f50421a35530")
     const weatherInfo = await response.json();
     this.$store.commit('SET_WEATHER_INFO', weatherInfo)
+    this.weatherLoaded = true;
   }
 };
 </script>
